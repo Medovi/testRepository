83b759ee4d499d59602e0cc1f1ae36d869a6551c
Medovi <sawawa2001@mail.ru>
2021-07-04 02:49:42 +0400

    добавил строгую почту

--- src/main/java/Progs/Service/UserService.java
+++ src/main/java/Progs/Service/UserService.java
@@ -34,7 +34,7 @@ public class UserService implements UserDetailsService {
             return false;
         }
 
-        user.setActive(true);
+        user.setActive(false);
         user.setRoles(Collections.singleton(Role.USER));
         user.getActivationCode(UUID.randomUUID().toString());
 
@@ -62,6 +62,7 @@ public class UserService implements UserDetailsService {
         }
 
         user.setActivationCode(null);
+        user.setActive(true);
 
         userRepository.save(user);
 
