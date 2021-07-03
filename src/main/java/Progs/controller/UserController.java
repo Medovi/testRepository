abc273590a52753fd1d87d4276042de338f100ba
dmitryvak1 <vakin2001@mail.ru>
2021-07-03 14:43:33 +0400

    конец 8.0

--- src/main/java/Progs/controller/UserController.java
+++ src/main/java/Progs/controller/UserController.java
@@ -1,10 +1,13 @@
 package Progs.controller;
 
+import Progs.Entity.Role;
+import Progs.Entity.User;
 import Progs.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,13 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "userList";//sdfasdfadfasfsdfasdfasdfafasdfa
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+
+        return "userEdit";
     }
 }
