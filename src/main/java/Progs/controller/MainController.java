8e73b25dfd4e4b46ab4b21ca88b45460e643067f
dmitryvak1 <vakin2001@mail.ru>
2021-07-04 15:54:41 +0400

    конец 13.0

--- src/main/java/Progs/controller/MainController.java
+++ src/main/java/Progs/controller/MainController.java
@@ -8,20 +8,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
+
 @Controller
 public class MainController {
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageRepository messageRepo;
 
     @Value("${upload.path}")
     private String uploadPath;
@@ -32,48 +36,61 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
-        Iterable<Message> messages = messageRepository.findAll();
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
 
-        if(filter != null && !filter.isEmpty()){
-            messages = messageRepository.findByTag(filter);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
         } else {
-            messages = messageRepository.findAll();
+            messages = messageRepo.findAll();
         }
 
         model.addAttribute("messages", messages);
         model.addAttribute("filter", filter);
+
         return "main";
     }
 
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model,
+            @Valid Message message,
+            BindingResult bindingResult,
+            Model model,
             @RequestParam("file") MultipartFile file
     ) throws IOException {
-        Message message = new Message(text, tag, user);
+        message.setAuthor(user);
 
-        if(file != null && !file.getOriginalFilename().isEmpty()){
-            File uploadDir = new File(uploadPath);
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
 
-            if(!uploadDir.exists()){
-                uploadDir.mkdir();
+            model.mergeAttributes(errorsMap);
+            model.addAttribute("message", message);
+        } else {
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+                File uploadDir = new File(uploadPath);
+
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+                file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+                message.setFilename(resultFilename);
             }
 
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+            model.addAttribute("message", null);
 
-            file.transferTo(new File(uploadPath + "/" + resultFileName));
-
-            message.setFilename(resultFileName);
+            messageRepo.save(message);
         }
 
-        messageRepository.save(message);
-        Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-        model.put("filter", "");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.addAttribute("messages", messages);
+
         return "main";
     }
 }
